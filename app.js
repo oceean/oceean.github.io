24e5ea0617f3a43d7016402d900b2e30d52f6f6f
Nikolay <billionspoint@gmail.com>
2019-02-13 19:51:12 +0300

    Convert news to Vue + creating Google tag

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 let app = new Vue({
     el: '#app',
-    data: {},
+    data: {
+        projects: [
+            {
+                name: "Thick Marker",
+                run: "https://thickmarker.oceean.store",
+                get: "https://github.com/oceean/ThickMarker"
+            },
+            {
+                name: "Sticker Columns",
+                run: "https://stickerscolumns.oceean.store",
+                get: "https://github.com/oceean/StickersColumns"
+            }
+        ]
+    },
 })
