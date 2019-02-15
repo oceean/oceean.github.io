67be5d62bda034c3807d77f6c7b4174374be592a
Nikolay <billionspoint@gmail.com>
2019-02-15 12:23:01 +0300

    Ajaxify data

--- app.js
+++ app.js
@@ -1,39 +1,74 @@
-function GS(catg, action) {
-    if ("ga" in window) {
-        tracker = ga.getAll()[0];
-        if (tracker)
-            tracker.send("event", catg, action);
-    }
-}
-
-function genGStatsAttn(text) {
-    return {
-        run: ["ProjectView", "test: " + text],
-        get: ["ProjectView", "code: " + text]
-    }
+function getLink(link, callback, errorcallback) {
+    let linker = new XMLHttpRequest();
+    linker.open("GET", link, true);
+    linker.timeout = 6000;
+    linker.onload = ()=>{
+        if (callback) {
+            callback(linker.responseText);
+        }
+    };
+    linker.onerror = errorcallback || (()=>{});
+    linker.send();
 }
 
 let app = new Vue({
     el: '#app',
     data: {
-        projects: [
-            {
-                name: "Thick Marker",
-                run: "https://thickmarker.oceean.store",
-                get: "https://github.com/oceean/ThickMarker",
-                stats: genGStatsAttn("ThickMarker")
+        showLogo: false,
+        columns: {
+            articles: {
+                size: 5,
+                color: "turquoise",
+                data: []
             },
-            {
-                name: "Sticker Columns",
-                run: "https://stickerscolumns.oceean.store",
-                get: "https://github.com/oceean/StickersColumns",
-                stats: genGStatsAttn("StickersColumns")
+            abouts: {
+                size: 4,
+                color: "cyan",
+                data: []
+            },
+            projects: {
+                size: 3,
+                color: "blue",
+                data: []
             }
-        ]
+        },
+        loading: {
+            articles: true,
+            abouts: true,
+            projects: true,
+        },
     },
     methods: {
         GSend: function (sendargs) {
-            GS(sendargs[0], sendargs[1])
+            if ("ga" in window) {
+                tracker = ga.getAll()[0];
+                if (tracker)
+                    tracker.send("event", sendargs[0], sendargs[1]);
+            }
+        },
+        lorem: function (l) {
+            let lorem = ""
+            for (let i = 0; i < l; i++) {
+                lorem += "loa ding "
+            }
+            return lorem
         }
     }
 })
+
+function ajax(what) {
+    getLink(`/data/${what}.json`, (data)=>{
+        app.loading[what] = false;
+        app.columns[what].data = JSON.parse(data);
+    })
+}
+ajax("articles");
+ajax("abouts");
+ajax("projects");
+
+let h = window.document.getElementsByTagName("header")[0];
+let i = 0;
+setInterval(()=>{
+    i++;
+    h.style.backgroundPositionX = `${-i}px`;
+}, 400)
